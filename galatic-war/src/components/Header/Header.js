42e74cdc41e0efba3886d50f413017d2774ab416
Vinícius Macêdo <142446793+Mithyaru@users.noreply.github.com>
2024-08-06 17:06:21 -0300

    Update Header.js

--- galatic-war/src/components/Header/Header.js
+++ galatic-war/src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import "./Header.css"
 import { Parallax } from 'react-parallax';
+import helldiversbg from '../../Assets/helldiversbg.png'
+import helldiverlogo from '../../Assets/helldivers-logo.png'
 
 const Header = () => {
     return (
         <Parallax
-        bgImage='../../Assets/helldiversbg.png'
+        bgImage={helldiversbg}
         bgImageStyle={{filter: 'blur(1px)'}}
         strength={500}
         >
         <div className="headerContent">
-            <img alt=" " src='../../Assets/helldivers-logo.png' width={550} height={180}></img>
+            <img alt=" " src={helldiverlogo} width={550} height={180}></img>
         </div>
         </Parallax>
     )
